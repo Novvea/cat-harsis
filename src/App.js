6f96bac220dbbf6adc01bc8e81cf1e87d80f1d97
Anna Novak <anna.c.novak@gmail.com>
2021-12-12 16:34:43 +0100

    Add linebreaks

--- src/App.js
+++ src/App.js
@@ -26,10 +26,13 @@ function App() {
       <h1>When you just need to color a cat</h1>
         <label for='bodycolor'>Choose color of body:</label>
         <input type='color' name='bodycolor' id='bodycolor' value={newColor.bodycolor} onChange={handleColorChange} />
+        <br/>
         <label for='earcolor'>Choose color of ears:</label>
         <input type='color' name='earcolor' id='earcolor' value={newColor.earcolor} onChange={handleColorChange} />
+        <br/>
         <label for='nosecolor'>Choose color of nose:</label>
         <input type='color' name='nosecolor' id='nosecolor' value={newColor.nosecolor} onChange={handleColorChange} />
+        <br/>
         <label for='eyecolor'>Choose color of eyes:</label>
         <input type='color' name='eyecolor' id='eyecolor' value={newColor.eyecolor} onChange={handleColorChange} />
       <div>
