ff78b323408b37fa2d03c8b8deda0b3b8be7dd64
Anna Novak <anna.c.novak@gmail.com>
2021-12-12 17:38:35 +0100

    Add semantic HTML structure

--- src/App.js
+++ src/App.js
@@ -24,23 +24,30 @@ function App() {
 
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>When you just need to color a cat</h1>
-        <label for='bodycolor' className={styles.lable}>Choose color of body:</label>
-        <input type='color' name='bodycolor' id='bodycolor' value={newColor.bodycolor} onChange={handleColorChange} />
-        <br/>
-        <label for='earcolor' className={styles.lable}>Choose color of ears:</label>
-        <input type='color' name='earcolor' id='earcolor' value={newColor.earcolor} onChange={handleColorChange} />
-        <br/>
-        <label for='nosecolor' className={styles.lable}>Choose color of nose:</label>
-        <input type='color' name='nosecolor' id='nosecolor' value={newColor.nosecolor} onChange={handleColorChange} />
-        <br/>
-        <label for='eyecolor' className={styles.lable}>Choose color of eyes:</label>
-        <input type='color' name='eyecolor' id='eyecolor' value={newColor.eyecolor} onChange={handleColorChange} />
-      <div>
-        <Cat color={color}/>
-      </div>
-    </div>
+    <section className={styles.container}>
+      <header>
+        <h1 className={styles.heading}>When you just need to color a cat</h1>
+      </header>
+{/*   <nav></nav> */}
+      <main>
+        <div className={styles.input_fields}></div>
+          <label for='bodycolor' className={styles.lable}>Choose color of body:</label>
+          <input type='color' name='bodycolor' id='bodycolor' value={newColor.bodycolor} onChange={handleColorChange} />
+          <br/>
+          <label for='earcolor' className={styles.lable}>Choose color of ears:</label>
+          <input type='color' name='earcolor' id='earcolor' value={newColor.earcolor} onChange={handleColorChange} />
+          <br/>
+          <label for='nosecolor' className={styles.lable}>Choose color of nose:</label>
+          <input type='color' name='nosecolor' id='nosecolor' value={newColor.nosecolor} onChange={handleColorChange} />
+          <br/>
+          <label for='eyecolor' className={styles.lable}>Choose color of eyes:</label>
+          <input type='color' name='eyecolor' id='eyecolor' value={newColor.eyecolor} onChange={handleColorChange} />
+        <div className={styles.cat_image}>
+          <Cat color={color}/>
+        </div>
+      </main>
+      {/* <footer></footer> */}
+    </section>
   );
 }
 
