10a917124c74086770f065d32ba4a5b9206db5f2
Anna Novak <anna.c.novak@gmail.com>
2021-12-21 13:27:23 +0100

    Add simple styling

--- src/App.js
+++ src/App.js
@@ -24,24 +24,30 @@ function App() {
 
 
   return (
-    <section className={styles.container}>
+    <section>
       <header>
-        <h1 className={styles.heading}>When you just need to color a cat</h1>
+        <h1>When you just need to color a cat</h1>
       </header>
 {/*   <nav></nav> */}
       <main>
-        <div className={styles.input_fields}></div>
-          <label for='bodycolor' className={styles.lable}>Choose color of body:</label>
-          <input type='color' name='bodycolor' id='bodycolor' value={newColor.bodycolor} onChange={handleColorChange} />
-          <br/>
-          <label for='earcolor' className={styles.lable}>Choose color of ears:</label>
-          <input type='color' name='earcolor' id='earcolor' value={newColor.earcolor} onChange={handleColorChange} />
-          <br/>
-          <label for='nosecolor' className={styles.lable}>Choose color of nose:</label>
-          <input type='color' name='nosecolor' id='nosecolor' value={newColor.nosecolor} onChange={handleColorChange} />
-          <br/>
-          <label for='eyecolor' className={styles.lable}>Choose color of eyes:</label>
-          <input type='color' name='eyecolor' id='eyecolor' value={newColor.eyecolor} onChange={handleColorChange} />
+        <ul className={styles.input_fields}>
+          <li>
+            <label for='bodycolor'>Choose color of body:</label>
+            <input type='color' name='bodycolor' id='bodycolor' value={newColor.bodycolor} onChange={handleColorChange} />
+          </li>
+          <li>
+            <label for='earcolor'>Choose color of ears:</label>
+            <input type='color' name='earcolor' id='earcolor' value={newColor.earcolor} onChange={handleColorChange} />
+          </li>
+          <li>
+            <label for='nosecolor'>Choose color of nose:</label>
+            <input type='color' name='nosecolor' id='nosecolor' value={newColor.nosecolor} onChange={handleColorChange} />
+          </li>
+          <li>
+            <label for='eyecolor'>Choose color of eyes:</label>
+            <input type='color' name='eyecolor' id='eyecolor' value={newColor.eyecolor} onChange={handleColorChange} />
+          </li>
+        </ul>
         <div className={styles.cat_image}>
           <Cat color={color}/>
         </div>
